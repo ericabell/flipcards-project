90e67f0433d0e2d193eea2907a2f73f88aa5b675
Eric Abell <ericabell@users.noreply.github.com>
2017-09-07 11:51:39 -0500

    Login/Logout works

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Flipcard' });
+  if( req.user) {
+    res.render('index', { title: 'Flipcard',
+                          user: req.user.name });
+  } else {
+    res.render('index', { title: 'Flipcard'
+                          });
+  }
+
 });
 
 module.exports = router;
